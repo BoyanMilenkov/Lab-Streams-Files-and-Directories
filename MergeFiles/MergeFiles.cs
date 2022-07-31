ab007341aa146c3c6a15cfc26b769a96398d2a48
BoyanMilenkov <80035234+BoyanMilenkov@users.noreply.github.com>
2022-07-31 12:12:12 +0300

    Add files via upload

---
+++ MergeFiles/MergeFiles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+namespace MergeFiles
+{
+    public class MergeFiles
+    {
+        public static void Main(string[] args)
+        {
+            var list = File.ReadAllLines("E:/BOBO/C#/MergeFiles/inputOne.txt").ToList();
+            list.AddRange(File.ReadAllLines("E:/BOBO/C#/MergeFiles/inputTwo.txt"));
+            list.Sort();
+            File.WriteAllLines("E:/BOBO/C#/MergeFiles/result.txt", list);
+        }
+    }
+}
